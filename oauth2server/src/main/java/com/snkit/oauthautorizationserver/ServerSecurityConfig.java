8ac0b9deed1a31be1d22ae779277f6b9d36d3962
ksr4innovation <kollu.srinu@gmail.com>
2020-01-26 16:09:24 +0530

    oauth2 local

---
+++ oauth2server/src/main/java/com/snkit/oauthautorizationserver/ServerSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.snkit.oauthautorizationserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) 
+      throws Exception {
+		 auth.inMemoryAuthentication().withUser("sairam").password("{bcrypt}$2a$10$Cm6ET9Tys2DK2PbOUDe0MuvuqIZOBo/sKoxV.FIhUNDkeBMbgSb0a").authorities("REPORTS")
+		    .and().withUser("ramsai").password("{bcrypt}$2a$10$Cm6ET9Tys2DK2PbOUDe0MuvuqIZOBo/sKoxV.FIhUNDkeBMbgSb0a").authorities("ADMIN");
+    }
+ 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() 
+      throws Exception {
+        return super.authenticationManagerBean();
+    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().permitAll();
+    }
+}
